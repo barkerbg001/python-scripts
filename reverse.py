1facc08046a7449400bbda3ba2c8825dc75bbb0f
Bradley <barkerbg001@gmail.com>
2025-06-18 20:50:29 +0200

    Add reverse string functionality with example usage

---
+++ reverse.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    return s[::-1]
+
+theString = "SpiderCode is awesome!"
+reverse_theString = reverse_string(theString)
+print(f"Original String: {theString}")
+print(f"Reversed String: {reverse_theString}")
